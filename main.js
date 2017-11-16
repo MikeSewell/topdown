b8c1027634eaa4cada232838c69d7a74229bf7f5
Michael  Sewell <mikesewell01@gmail.com>
2017-11-16 01:29:43 -0500

    added bug fix of load

--- main.js
+++ main.js
@@ -10,23 +10,24 @@ class Run {
         this.map = {};
         this.markers = []
         this.zipArr = []
-        // this.lat = "test"
-        // this.lng = 0
+        this.test = "test"
+        this.lat = ""
+        this.lng = ""
+        localStorage.setItem("tempList", "")
+        localStorage.setItem("lowF", "")
+        localStorage.setItem("highF", "")
+        localStorage.setItem("lowC", "")
+        localStorage.setItem("highC", "")
+        localStorage.setItem("currTempF", "")
+        localStorage.setItem("currTempC", "")
+        localStorage.setItem("currIcon", "")
         this.setting()
     }
+    // setting screen
     setting() {
         let geo = document.querySelector("#geoLoc")
-        // if (localStorage.getItem("geo")) {
-        //     if (localStorage.getItem("geo") == "true") {
-        //         geo.checked = true
-        //     } else {
-        //         geo.checked = false
-        //     }
-        // } else {
-        //     localStorage.setItem("geo", "true")
-        // }
 
-        // geoLocation 
+        // geoLocation toggle switch
         geo.addEventListener("click", e => {
             if (geo.checked) {
                 localStorage.setItem("geo", "true")
@@ -34,21 +35,21 @@ class Run {
                 localStorage.setItem("geo", "false")
             }
         })
+        // geo location toggle on return
         if (localStorage.getItem("geo") == "true" || (!localStorage.getItem("geo"))) {
-            // ================================
-            this.dosomthing()
-            // ================================
-            // this.nav()
-            // this.init_map()
-            // this.changeZip()
+            this.getshowData(true)
+            this.nav()
+            this.init_map()
+            this.changeZip()
         } else {
-            // localStorage.setItem("geo", "false")
- 
-            // this.nav()
-            // this.init_map()
-            // this.changeZip()
+            geo.checked = false
+            this.getshowData(false)
+            this.nav()
+            this.init_map()
+            this.changeZip()
         }
     }
+
     // navigation 
     nav() {
         let dash = document.querySelector("#dashboard")
@@ -66,74 +67,93 @@ class Run {
         })
 
     }
+
     // get geo location and populate data
-    dosomthing() {
+    getshowData(runGeo) {
+
+        // show data after all data is loaded
+        let count = 1
+        document.addEventListener("currTemphilo", e => {
+            if (count == 3) {
+                showData()
+            }
+            count++
+        })
+        document.addEventListener("currTempSet", e => {
+            if (count == 3) {
+                showData()
+            }
+            count++
+        })
+        document.addEventListener("currTemphouly", e => {
+            if (count == 3) {
+                showData()
+            }
+            count++
+        })
+
+        //  get current high and low temp
         function getHL(lat, lng) {
             let url = 'http://api.wunderground.com/api/47e6f06078fb48ae/forecast/q/' + lat + ',' + lng + '.json'
             fetch(url).then(function (response) {
-                // Convert to JSON
                 return response.json();
             }).then(function (j) {
-                console.log(j);
                 localStorage.setItem('lowF', j.forecast.simpleforecast.forecastday[0].low.fahrenheit);
                 localStorage.setItem('highF', j.forecast.simpleforecast.forecastday[0].high.fahrenheit);
                 localStorage.setItem('lowC', j.forecast.simpleforecast.forecastday[0].low.celsius);
                 localStorage.setItem('highC', j.forecast.simpleforecast.forecastday[0].high.celsius);
-                localStorage.setItem('tempformat', true);
-                // console.log("test this lat",this.lat); cannot read
-                
+                let evt = new Event("currTemphilo")
+                document.dispatchEvent(evt)
                 return
             })
-            
+
         }
 
+        // get current temp
         function getCurrTemp(lat, lng) {
             let url = "http://api.wunderground.com/api/47e6f06078fb48ae/conditions/q/" + lat + "," + lng + ".json"
             fetch(url).then(function (response) {
-                // Convert to JSON
                 return response.json();
             }).then(function (j) {
-                console.log("getCurrTemp", j);
-                localStorage.setItem('currIcon', j.current_observation.icon_url);
+                localStorage.setItem('currIcon', j.current_observation.icon);
                 localStorage.setItem('currTempF', j.current_observation.feelslike_f);
                 localStorage.setItem('currTempC', j.current_observation.feelslike_c);
+                let evt = new Event("currTempSet")
+                document.dispatchEvent(evt)
                 return
             })
         }
 
+        // get all hourly temps
         function getHourly(lat, lng) {
             let url = "http://api.wunderground.com/api/47e6f06078fb48ae/hourly/q/" + lat + "," + lng + ".json"
             return fetch(url).then(function (response) {
-                // Convert to JSON
                 return response.json();
             }).then(function (j) {
                 localStorage.setItem("tempList", JSON.stringify(j))
             }).then(function () {
-                showData()
+                let evt = new Event("currTemphouly")
+                document.dispatchEvent(evt)
             })
         }
 
         function showData() {
-            //header
+            // populate header
             let dash = document.querySelector("#main")
-            // document.querySelector("#currTemp").src = localStorage.getItem("currIcon")
-            // document.querySelector("#high").innerHTML = localStorage.getItem("highF")
-            // document.querySelector("#low").innerHTML = localStorage.getItem("lowF")
+            document.querySelector("#currImg").src = `./css/iconpack/day/${localStorage.getItem("currIcon")}.png`
+            document.querySelector("#high").innerHTML = localStorage.getItem("highF")
+            document.querySelector("#low").innerHTML = localStorage.getItem("lowF")
 
-            dash.insertAdjacentHTML("afterbegin", `<p id="high">${localStorage.getItem("highF")}</p>`)
-            dash.insertAdjacentHTML("afterbegin", `<p id="currTemp"> <img src="${localStorage.getItem("currIcon")}" alt=""></p>`)
-            dash.insertAdjacentHTML("afterbegin", `<p id="low">${localStorage.getItem("lowF")}</p>`)
-
-            //hourly list
+            // populate hourly list
             let list = document.querySelector("#hourTable")
+            list.innerHTML = ""
             let hour = JSON.parse(localStorage.getItem("tempList"))
-            console.log(hour);
 
             for (var i = 0; i < 9; i++) {
                 list.insertAdjacentHTML("beforeend", `<tr>
                         <td id="hrt${i}">${hour.hourly_forecast[i].FCTTIME.civil}</td>
                         <td id="temp${i}">${hour.hourly_forecast[i].temp.english} F</td>
-                        <td><img src="${hour.hourly_forecast[i].icon_url}" alt="weathericon"></td>
+                        <td><img class="weatherIcon" src="./css/iconpack/day/${hour.hourly_forecast[i].icon}.png" alt="weathericon"></td>
                         <td>${hour.hourly_forecast[i].wspd.english} ${hour.hourly_forecast[i].wdir.dir}</td>
                     </tr>`)
             }
@@ -142,18 +162,12 @@ class Run {
             let tempSelect = document.querySelector("#tempSelect")
             tempSelect.addEventListener("click", e => {
                 if (tempSelect.checked) {
-                    localStorage.setItem('tempformat', false);
-
                     document.querySelector("#low").innerHTML = localStorage.getItem("lowC")
                     document.querySelector("#high").innerHTML = localStorage.getItem("highC")
-
-
                     for (var i = 0; i < 9; i++) {
                         document.querySelector(`#temp${i}`).innerHTML = hour.hourly_forecast[i].temp.metric + " C"
                     }
                 } else {
-                    localStorage.setItem('tempformat', true);
-
                     document.querySelector("#low").innerHTML = localStorage.getItem("lowF")
                     document.querySelector("#high").innerHTML = localStorage.getItem("highF")
                     for (var i = 0; i < 9; i++) {
@@ -161,6 +175,7 @@ class Run {
                     }
                 }
             })
+
             //timeformat toggle
             let timeSelect = document.querySelector("#timeSelect")
             timeSelect.addEventListener("click", e => {
@@ -170,105 +185,105 @@ class Run {
                             document.querySelector(`#hrt${i}`).innerHTML = "0" + hour.hourly_forecast[i].FCTTIME.hour + "00"
                         } else {
                             document.querySelector(`#hrt${i}`).innerHTML = hour.hourly_forecast[i].FCTTIME.hour + "00"
-
                         }
                     }
                 } else {
                     for (var i = 0; i < 9; i++) {
                         document.querySelector(`#hrt${i}`).innerHTML = hour.hourly_forecast[i].FCTTIME.civil
-
                     }
                 }
             })
         }
 
-        // +++++++++++++++++++++
-
+        // run functions async
         let lat;
         let lng;
-
         const options = {
             method: 'Post'
         };
         let url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAKTaI3tOSjLN-HO2XC0BOKbSWHa1zHWdY"
-        fetch(url, options).then(function (response) {
-            // Convert to JSON
-            return response.json();
-        }).then(function (d) {
-            console.log('promiseGeo', d);
-            lat = d.location.lat.toFixed(3)
-            lng = d.location.lng.toFixed(3)
-            let geo = [lat, lng]
-            return geo
-        }).then(function (result) {
-            console.log('promisCurrtemp', result);
-            return getHL(lat, lng)
-        }).then(function (result) {
-            // console.log('promisgetHl', result);
-            return getCurrTemp(lat, lng)
-        }).then(function (result) {
-            // console.log('promisGetHouly', result);
-            // console.log(lat);
-            return getHourly(lat, lng)
-        })
 
-        // console.log(lng);
-        // this.lat = lat
-        // this.lng = lng
-        
+        // geo location toggle
+        if (runGeo) {
 
+            fetch(url, options).then(function (response) {
+                return response.json();
+            }).then(function (d) {
+                lat = d.location.lat.toFixed(3)
+                lng = d.location.lng.toFixed(3)
+                localStorage.setItem("lat", lat)
+                localStorage.setItem("lng", lng)
+                let geo = ["", lat, lng]
+                return geo
+            }).then(function (result) {
+                return getHL(lat, lng)
+            }).then(function (result) {
+                return getCurrTemp(lat, lng)
+            }).then(function (result) {
+                return getHourly(lat, lng)
+            })
+        } else {
+            fetch(url, options).then(function (response) {
+                return
+            }).then(function (d) {
+                lat = localStorage.getItem("lat")
+                lng = localStorage.getItem("lng")
+                let geo = ["", lat, lng]
+                return geo
+            }).then(function (result) {
+                return getHL(lat, lng)
+            }).then(function (result) {
+                return getCurrTemp(lat, lng)
+            }).then(function (result) {
+                return getHourly(lat, lng)
+            })
+
+        }
     }
 
     // change zip for both zip
     changeZip() {
         let zipInput = document.querySelector("#zipChange")
-        zipInput.addEventListener("keyup", e => {
+        zipInput.addEventListener("keyup", e => { // zip for weather
             if (zipInput.value.length == 5 && (!isNaN(zipInput.value))) {
-                this.ZipToLL(zipInput.value)
                 localStorage.setItem("geo", "false")
                 document.querySelector("#geoLoc").checked = false
-                this.getHL()
-                this.getCurrTemp()
-                this.getHourly()
-                location.reload()
+                this.ZipToLL(zipInput.value, this.getshowData)
             }
         })
-        let addZip = document.querySelector("#addZip")
+        let addZip = document.querySelector("#addZip") // zip for map
         document.querySelector("#favZip button").addEventListener("click", e => {
             if (addZip.value.length == 5 && (!isNaN(addZip.value))) {
                 this.ZipToLL(addZip.value, () => {
                     this.addMarkerToMap()
                 })
             }
+            addZip.value = ""
         })
     }
     //convert zip to lat,long
     ZipToLL(zip, callback) {
 
         let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${zip}&key=AIzaSyDCbLmYX5sRQJ_KgkYgD0mZ2qwBOpXuzSE`;
-
+        let run = "now"
+        let lat = 0
+        let lng = 0
         fetch(url).then(function (response) {
-            return response.json();
-        }).then(function (d) {
-
-            let lat = d.results[0].geometry.location.lat.toFixed(2)
-            let lng = d.results[0].geometry.location.lng.toFixed(2)
-            localStorage.setItem('lat', lat)
-            localStorage.setItem('lng', lng)
-
-            console.log('promiseZiptoll', d);
-            lat = d.location.lat.toFixed(3)
-            lng = d.location.lng.toFixed(3)
-            let geo = [lat, lng]
-            return geo
-        })
+                return response.json();
+            })
+            .then(function (d) {
+                localStorage.setItem("lat", d.results[0].geometry.location.lat.toFixed(3))
+                localStorage.setItem("lng", d.results[0].geometry.location.lng.toFixed(3))
+                callback(false)
+                return
+            })
     }
     // build map
-    init_map() {
+    init_map(lat = 28.542, lng = -81.376) {
         var var_location = new google.maps.LatLng(localStorage.getItem("lat"), localStorage.getItem("lng"))
         var var_mapoptions = {
             center: var_location,
-            zoom: 13,
+            zoom: 10,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             mapTypeControl: false,
             panControl: false,
@@ -284,33 +299,22 @@ class Run {
             position: var_location,
             map: this.map
         })
-
-        if (localStorage.getItem("markers")) {
-            let data = localStorage.getItem("markers").split("|")
-            console.log(data);
-            console.log(data.length);
-            console.log(data[0].length);
-        }
     }
 
-
     // add marker to map
-    // addMarkerToMap() {
-    //     let latt = parseFloat(localStorage.getItem('userlat'))
-    //     let lngt = parseFloat(localStorage.getItem('userlng'))
+    addMarkerToMap() {
+        let latt = parseFloat(localStorage.getItem('lat'))
+        let lngt = parseFloat(localStorage.getItem('lng'))
 
-    //     let lat = latt.toFixed(1)
-    //     let lng = lngt.toFixed(1)
+        let lat = latt.toFixed(1)
+        let lng = lngt.toFixed(1)
 
-    //     var myLatLng = new google.maps.LatLng(lat, lng);
-    //     var marker = new google.maps.Marker({
-    //         position: myLatLng,
-    //         map: this.map,
-    //         animation: google.maps.Animation.DROP
-    //     });
-
-    //     this.markers.push(lat, lng + "|")
-    //     this.map.panTo(myLatLng)
-    //     localStorage.setItem("markers", this.markers)
-    // }
+        var myLatLng = new google.maps.LatLng(lat, lng);
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: this.map,
+            animation: google.maps.Animation.DROP
+        });
+        this.map.panTo(myLatLng)
+    }
 }
